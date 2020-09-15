12ee9ab2a087fedb432ce995beae9229768455e4
drkphnx137 <drkphnx137@gmail.com>
2020-09-15 11:51:18 -0400

    added javascript to change player picture based on dropdown choice

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function selectWeapon() {
-	var dropdown = document.querySelector('.dropdown');
+	var pngs = [
+		["srcFiles/Elements/rock.png"],
+		["srcFiles/Elements/paper.png"],
+		["srcFiles/Elements/paper.png"]
+	];
 
-	
+	var dropdownValue = document.querySelector(".dropdown").value;
+	var picSrc = document.querySelector(".player-choice.pic");
+
+	if(document.querySelector(".dropdown").value === "rock") {
+		document.querySelector(".player-choice-pic").src = 
+		"srcFiles/Elements/rock.png";
+	} else if(document.querySelector(".dropdown").value === "paper") {
+		document.querySelector(".player-choice-pic").src =
+		"srcFiles/Elements/paper.png"
+	} else if(document.querySelector(".dropdown").value === "scissors") {
+		document.querySelector(".player-choice-pic").src =
+		"srcFiles/Elements/scissors.png"
+	}
 }
