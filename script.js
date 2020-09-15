f12fa0e2ce08edcb9f260c4d9916dd7c2208021a
drkphnx137 <drkphnx137@gmail.com>
2020-09-15 14:15:56 -0400

    updated some of the CSS.  Started work on computer choice JS

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 function selectWeapon() {
-	var pngs = [
-		["srcFiles/Elements/rock.png"],
-		["srcFiles/Elements/paper.png"],
-		["srcFiles/Elements/paper.png"]
-	];
-
 	var dropdownValue = document.querySelector(".dropdown").value;
 	var picSrc = document.querySelector(".player-choice.pic");
 
 	if(document.querySelector(".dropdown").value === "rock") {
 		document.querySelector(".player-choice-pic").src = 
 		"srcFiles/Elements/rock.png";
-	} else if(document.querySelector(".dropdown").value === "paper") {
+	} else if(document.querySelector(".dropdown").value === 
+		"paper") {
 		document.querySelector(".player-choice-pic").src =
 		"srcFiles/Elements/paper.png"
-	} else if(document.querySelector(".dropdown").value === "scissors") {
+	} else if(document.querySelector(".dropdown").value === 
+		"scissors") {
 		document.querySelector(".player-choice-pic").src =
 		"srcFiles/Elements/scissors.png"
+	} else if(document.querySelector(".dropdown").value === 
+		"default") {
+		document.querySelector(".player-choice-pic").src =
+		""
+	}
+}
+
+function shoot() {
+	var weapons = ["rock", "paper", "scissors"];
+	var compChoice = weapons[Math.floor(Math.random() * (3 - 0))]
+	console.log(compChoice);
+	if(compChoice === "rock") {
+		document.querySelector(".computer-choice-pic").src = 
+		"srcFiles/Elements/rock.png";
+	} else if(compChoice === "paper") {
+		document.querySelector(".computer-choice-pic").src = 
+		"srcFiles/Elements/paper.png";
+	} else if(compChoice === "scissors") {
+		document.querySelector(".computer-choice-pic").src = 
+		"srcFiles/Elements/scissors.png";
 	}
 }
